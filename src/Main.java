7d15121cafe9897cb2b4bd2e6c9ba4a137decf22
antyaroslav <antoyaroslav@gmail.com>
2026-01-22 17:01:57 +0300

    renew

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int miles = service.calculate(price); // должно получиться 500
+        int miles = service.calculate(price);
         System.out.println(miles);
+        System.out.println(service.calculate(1_000));
+        System.out.println(service.calculate(20_000));  
+        System.out.println(service.calculate(0)); 
+
     }
 }
